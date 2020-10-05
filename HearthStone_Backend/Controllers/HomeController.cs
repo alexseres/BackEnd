84adfec6e3a8e5ec628a2d13978fdafd3658b520
HaveaKillerDay <45069029+HaveaKillerDay@users.noreply.github.com>
2020-10-05 15:45:32 +0200

    Basic API key

--- HearthStone_Backend/Controllers/HomeController.cs
+++ HearthStone_Backend/Controllers/HomeController.cs
@@ -9,7 +9,9 @@ using HearthStone_Backend.Models;
 
 namespace HearthStone_Backend.Controllers
 {
-    public class HomeController : Controller
+    [Route("api")]
+    [ApiController]
+    public class HomeController : ControllerBase
     {
         private readonly ILogger<HomeController> _logger;
 
@@ -17,21 +19,7 @@ namespace HearthStone_Backend.Controllers
         {
             _logger = logger;
         }
+        
 
-        public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
