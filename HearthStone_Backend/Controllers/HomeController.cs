132ec636ca2dbad78fa0f80d0beaf9739ae98007
HaveaKillerDay <45069029+HaveaKillerDay@users.noreply.github.com>
2020-10-20 14:06:59 +0200

    fill up is ready to go

--- HearthStone_Backend/Controllers/HomeController.cs
+++ HearthStone_Backend/Controllers/HomeController.cs
@@ -9,6 +9,7 @@ using HearthStone_Backend.Models;
 using Microsoft.AspNetCore.Http;
 using Newtonsoft.Json.Linq;
 using HearthStone_Backend.Services;
+using HearthStone_Backend.Migrations;
 
 namespace HearthStone_Backend.Controllers
 {
@@ -17,12 +18,12 @@ namespace HearthStone_Backend.Controllers
     [ApiController]
     public class HomeController : ControllerBase
     {
-        private readonly APIfetcher _contextNEW;
+        private readonly APIfetcher _apiFetcher;
         private readonly ICardRepository _cardRepository;
 
         public HomeController(ICardRepository repository, APIfetcher apiFetcher)
         {
-            _contextNEW = apiFetcher;
+            _apiFetcher = apiFetcher;
             _cardRepository = repository;
 
         }
@@ -30,32 +31,49 @@ namespace HearthStone_Backend.Controllers
         [HttpPost("search")]
         public async Task<List<Card>> GetAskedCardForSearch([FromBody]string data)
         {
-            List<Card> list = _contextNEW.CardsList;
+            List<Card> list = (List<Card>)_cardRepository.GetCards();
             List<Card> expectedResults = list.FindAll(x => x.Name.Contains(data));
             return expectedResults;
         }
-        
-        
+
+        //If DB is filled succesfully, replace the method with ONLY this: 
+        //     return _cardRepository.GetCards().Take(50).ToList();
+
         [HttpGet("list")]
         public async Task<List<Card>>  GetHomePageData()
         {
-            var result = _contextNEW.CardsList.Take(25).ToList();
-            return result;
+
+            Dictionary<string, List<Card>> apiResult = await _apiFetcher.GetCards();
+
+            foreach (List<Card> list in apiResult.Values)
+            {
+                _cardRepository.AddCards(list);
+            }
+
+            return _cardRepository.GetCards().Take(50).ToList();
         }
         
         [HttpGet("info")]
         public async Task<Info> GetInfoToHomePage()
         {
-
-            var result =  _contextNEW.InfoContents;
+            Info result = await _apiFetcher.GetInfoToHomePage();
+            
             return result;
         }
+
+
+        //If DB is filled succesfully, replace the method with ONLY this: 
+        //     return _cardRepository.GetCardBacks().ToList();
+
 
         [HttpGet("cards-back")]
         public async Task<List<CardBack>> GetCardsBackData()
         {
-            var result = _contextNEW.CardBackList;
-            return result;
+            List<CardBack> result = await _apiFetcher.GetBackCards();
+
+            _cardRepository.AddCardBacks(result);
+
+            return _cardRepository.GetCardBacks().ToList();
         }
     }
 }
